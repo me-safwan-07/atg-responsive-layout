5c9c98ad6870f792f351f1d0f702708f34aef44c
me-safwan-07 <mesafwan07@gmail.com>
2025-02-07 17:36:22 +0530

    installed the vite-react and Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div className="container mt-4">
+        <h1>Welcome to ATG Responsive Layout</h1>
+        <p>This is a Bootstrap-integrated React project.</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
